a8df52374e0aff309949f1a880c267f31c8de31d
ngaukonojane <namtendaisabella@gmail.com>
2020-10-27 14:34:35 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // slick carousel
+    $('.slider').slick({
+      dots: true,
+      vertical: false,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      verticalSwiping: true,
+    });
+  });
